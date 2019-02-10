ed96c36da977fca497173a6f17a10350a577b6ea
Kevin Liao <kliao2016@users.noreply.github.com>
2019-02-10 15:13:58 -0500

    Update Camera.swift

--- Camera.swift
+++ Camera.swift
@@ -12,39 +12,44 @@ class ViewController: UIViewController {
    
    override func viewDidLoad() {
       super.viewDidLoad()
-     //Initialize session an output variables this is necessary
-     session = AVCaptureSession()
-     output = AVCaptureStillImageOutput()
-     let camera = getDevice(.Back)
-     do {
-        input = try AVCaptureDeviceInput(device: camera)
-     } catch let error as NSError {
-        print(error)
-        input = nil
-     }
-     if(session?.canAddInput(input) == true){
-        session?.addInput(input)
-        output?.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
-        if(session?.canAddOutput(output) == true){
-           session?.addOutput(output)
-           previewLayer = AVCaptureVideoPreviewLayer(session: session)
-           previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
-           previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.Portrait
-           previewLayer?.frame = cameraPreview.bounds
-           cameraPreview.layer.addSublayer(previewLayer!)
-           session?.startRunning()
-        }
-     }
-  }
-//Get the device (Front or Back)
-func getDevice(position: AVCaptureDevicePosition) -> AVCaptureDevice? {
-   let devices: NSArray = AVCaptureDevice.devices();
-   for de in devices {
-      let deviceConverted = de as! AVCaptureDevice
-      if(deviceConverted.position == position){
-         return deviceConverted
+      
+      //Initialize session an output variables this is necessary
+      session = AVCaptureSession()
+      output = AVCaptureStillImageOutput()
+      let camera = getDevice(.Back)
+      
+      do {
+         input = try AVCaptureDeviceInput(device: camera)
+      } catch let error as NSError {
+         print(error)
+         input = nil
+      }
+      
+      if (session?.canAddInput(input)) {
+         session?.addInput(input)
+         output?.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
+
+         if (session?.canAddOutput(output)) {
+            session?.addOutput(output)
+            previewLayer = AVCaptureVideoPreviewLayer(session: session)
+            previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+            previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.Portrait
+            previewLayer?.frame = cameraPreview.bounds
+            cameraPreview.layer.addSublayer(previewLayer!)
+            session?.startRunning()
+         }
       }
    }
-   return nil
-}
+   
+   //Get the camera (Front or Back)
+   func getDevice(position: AVCaptureDevicePosition) -> AVCaptureDevice? {
+      let devices: NSArray = AVCaptureDevice.devices();
+      for device in devices {
+         let deviceConverted = device as! AVCaptureDevice
+         if(deviceConverted.position == position){
+            return deviceConverted
+         }
+      }
+      return nil
+   }
 }
