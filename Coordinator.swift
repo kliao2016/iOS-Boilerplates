611466af3bbc5c0d5f2d720e0fc6718113ad953d
Kevin Liao <kliao2016@users.noreply.github.com>
2020-08-19 22:25:26 -0600

    Update Coordinator.swift

--- Coordinator.swift
+++ Coordinator.swift
@@ -1,54 +1,90 @@
 import UIKit
 
-// Make sure Coordinator has to be a class because it is passed around so much
-protocol Coordinator: class {
-    var childCoordinators: [Coordinator] { get set }
-    var navigationController: UINavigationController { get set }
+class Coordinator: NSObject {
 
-    func start()
-}
-
-class MainCoordinator: Coordinator {
-    var childCoordinators = [Coordinator]()
-    var navigationController: UINavigationController
-
-    init(navigationController: UINavigationController) {
-        self.navigationController = navigationController
-    }
+    weak var parentCoordinator: Coordinator?
+    
+    var childCoordinators: [Coordinator] = []
 
     func start() {
-        let vc = ViewController.instantiate()
-        navigationController.pushViewController(vc, animated: false)
-    }
-    
-    // Example view controllers being managed by MainCoordinator
-    func buySubscription() {
-        let vc = BuyViewController.instantiate()
-        vc.coordinator = self
-        navigationController.pushViewController(vc, animated: true)
+        preconditionFailure("This method needs to be overriden by concrete subclass.")
     }
 
-    func createAccount() {
-        let vc = CreateAccountViewController.instantiate()
-        vc.coordinator = self
-        navigationController.pushViewController(vc, animated: true)
+    func finish() {
+        preconditionFailure("This method needs to be overriden by concrete subclass.")
     }
+
+    func addChildCoordinator(_ coordinator: Coordinator) {
+        childCoordinators.append(coordinator)
+    }
+
+    func removeChildCoordinator(_ coordinator: Coordinator) {
+        if let index = childCoordinators.firstIndex(of: coordinator) {
+            childCoordinators.remove(at: index)
+        } else {
+            print("Couldn't remove coordinator: \(coordinator). It's not a child coordinator.")
+        }
+    }
+
+    func removeAllChildCoordinatorsWith<T>(type: T.Type) {
+        childCoordinators = childCoordinators.filter { $0 is T  == false }
+    }
+
+    func removeAllChildCoordinators() {
+        childCoordinators.removeAll()
+    }
+
 }
 
-func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // create the main navigation controller to be used for our app
-    let navController = UINavigationController()
+class MainTabCoordinator: Coordinator {
+    
+    let window: UIWindow?
+    
+    lazy var rootTabBarController: UITabBarController = {
+        return tabBarController
+    }()
+    
+    let tabBarController: UITabBarController
+    
+    let drillsCoordinator: DrillsCoordinator
+    let dashboardCoordinator: DashboardCoordinator
+    let analysisCoordinator: AnalysisCoordinator
 
-    // send that into our coordinator so that it can display view controllers
-    coordinator = MainCoordinator(navigationController: navController)
+    // MARK: - Coordinator
+    init(window: UIWindow?) {
+        self.window = window
+        
+        tabBarController = UITabBarController()
+        
+        drillsCoordinator = DrillsCoordinator()
+        dashboardCoordinator = DashboardCoordinator()
+        analysisCoordinator = AnalysisCoordinator()
+        
+        let drillsNavController = drillsCoordinator.rootNavController
+        drillsNavController.tabBarItem = UITabBarItem(title: "Drills", image: UIImage(systemName: "square.and.arrow.up"), selectedImage: UIImage(systemName: "square.and.arrow.up.fill"))
+        
+        let dashboardNavController = dashboardCoordinator.rootNavController
+        dashboardNavController.tabBarItem = UITabBarItem(title: "Dashboard", image: UIImage(systemName: "square.and.arrow.up"), selectedImage: UIImage(systemName: "square.and.arrow.up.fill"))
+        
+        let analysisNavController = analysisCoordinator.rootNavController
+        analysisNavController.tabBarItem = UITabBarItem(title: "Analysis", image: UIImage(systemName: "square.and.arrow.up"), selectedImage: UIImage(systemName: "square.and.arrow.up.fill"))
+        
+        super.init()
+        childCoordinators.append(drillsCoordinator)
+        childCoordinators.append(dashboardCoordinator)
+        childCoordinators.append(analysisCoordinator)
+        
+        tabBarController.viewControllers = [drillsNavController, dashboardNavController, analysisNavController]
+        tabBarController.tabBar.isTranslucent = false
+        tabBarController.delegate = self
+    }
 
-    // tell the coordinator to take over control
-    coordinator?.start()
+    override func start() {
+        guard let window = window else {
+            return
+        }
 
-    // create a basic UIWindow and activate it
-    window = UIWindow(frame: UIScreen.main.bounds)
-    window?.rootViewController = navController
-    window?.makeKeyAndVisible()
-
-    return true
+        window.rootViewController = rootTabBarController
+        window.makeKeyAndVisible()
+    }
 }
